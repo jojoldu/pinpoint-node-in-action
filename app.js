ccaf141352217570882945caa7128562650d6ad9
jojoldu <jojoldu@gmail.com>
2021-05-15 23:00:25 +0900

    테스트용 호출 코드 추가
    * 외부 API 호출까지 trace 되는지
    * 외부 function 호출까지 trace 되는지

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('pinpoint-node-agent'); // pinpoint node agent
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +23,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  axios.get(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories`)
+      .then(function (response) {
+        console.log(`response ${response}`)
+        res.render('index', { title: 'Express' });
+      })
 });
+
+router.get('/2', function(req, res, next) {
+    console.log('call: /2');
+    call(res);
+});
+
+function call(res) {
+    axios.get(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories`)
+        .then(function (response) {
+            console.log(`response ${response}`)
+            res.render('index', {title: 'Express'});
+        })
+}
+
 
 module.exports = router;
